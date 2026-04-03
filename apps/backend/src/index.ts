b218654839049915c17a01d777fa76f9322cb4ec
Aman Mahato <mahatoaman2510@gmail.com>
2026-04-03 16:40:34 +0000

    modify import app in backend/src/index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { app } from "../http-backend/dist/index.js";
+import app from "../http-backend/dist/index.js";
 import { setupWebSocket } from "../ws-backend/dist/index.js";
 
 const PORT = process.env.PORT || 10000;
@@ -10,4 +10,4 @@ const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-setupWebSocket(server); 
+setupWebSocket(server);
